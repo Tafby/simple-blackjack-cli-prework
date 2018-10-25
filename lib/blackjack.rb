0d83c0caf1cd0199cae7b904b39b754961dc1e60
Tiffany Carter <tafby88@gmail.com>
2018-10-25 02:44:05 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,9 +25,9 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card
-  deal_card
-  display_card_total(card_total)
+  sum = deal_card + deal_card
+  display_card_total(sum)
+  return sum
   
 end
 
