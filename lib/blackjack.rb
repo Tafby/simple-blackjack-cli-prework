c1ef8e8d7d34202396b03cf9905c6e3faebaca6d
Tiffany Carter <tafby88@gmail.com>
2018-10-25 03:14:44 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,7 +51,7 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner(number)
+def runner(display_card_total)
   welcome
   initial_round
   hit?(display_card_total)
